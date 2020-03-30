237c6ca8a8e85d359f66f017b53c41b225a98c32
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-30 19:18:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,27 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    
+    get "/" do
+      "Welcome to the Nested Forms Lab!"
+    end
 
     # code other routes/actions here
+    get "/new" do
+      erb :new
+    end
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+     
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+     
+      @ships = Ship.all
+     
+      erb :show
+    end
 
   end
 end
